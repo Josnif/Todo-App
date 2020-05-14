fe52236bc601e7d3c58c2fabd6fb49d67c0ebfa0
Joenif <josephnifemi89@gmail.com>
2020-05-14 03:16:05 +0100

    initial commit

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header">
+        <a>Todo-List</a>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Header',
+}
+
+</script>
+
+<style scoped>
+    .header {
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding: 15px;
+    }
+
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+    }
+</style>
